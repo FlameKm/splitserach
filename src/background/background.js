3cc94672358a0b50f68c7ef6d4f521a2bd01efca
yuancan <huangyuancan@poinstspread.tech>
2026-01-16 11:25:14 +0800

    修改默认配置

--- src/background/background.js
+++ src/background/background.js
@@ -1,10 +1,9 @@
 const defaultEngines = [
   { name: 'Google', url: 'https://www.google.com/search?q=', pattern: 'google.com', trigger: true },
-  { name: 'Google.hk', url: 'https://www.google.com.hk/search?q=', pattern: 'google.com.hk', trigger: true },
   { name: '百度', url: 'https://www.baidu.com/s?wd=', pattern: 'baidu.com', trigger: true },
   { name: '必应', url: 'https://cn.bing.com/search?q=', pattern: 'bing.com', trigger: true },
-  { name: '360', url: 'https://www.so.com/s?q=', pattern: 'so.com', trigger: false },
-  { name: '搜狗', url: 'https://www.sogou.com/web?query=', pattern: 'sogou.com', trigger: false },
+  { name: '360', url: 'https://www.so.com/s?q=', pattern: 'so.com', trigger: true },
+  { name: '搜狗', url: 'https://www.sogou.com/web?query=', pattern: 'sogou.com', trigger: true },
   { name: '搜狗翻译', url: 'https://fanyi.sogou.com/?keyword=', pattern: 'fanyi.sogou.com', trigger: false },
   { name: '知乎', url: 'https://www.zhihu.com/search?q=', pattern: 'zhihu.com', trigger: false },
 ];
